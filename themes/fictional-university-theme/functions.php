31ff0d4aae1e83488d5023f219297e828f1651f9
Julian Chatterton <juleschatt@Julians-iMac.local>
2023-12-12 18:25:05 -0600

    new functions fix

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -13,15 +13,15 @@ function university_custom_rest() {
 add_action('rest_api_init', 'university_custom_rest');
 function pageBanner($args = NULL) {
   
-  if (!$args['title']) {
+  if (!isset($args['title'])) {
     $args['title'] = get_the_title();
   }
 
-  if (!$args['subtitle']) {
+  if (!isset($args['subtitle'])) {
     $args['subtitle'] = get_field('page_banner_subtitle');
   }
 
-  if (!$args['photo']) {
+  if (!isset($args['photo'])) {
     if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
       $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
     } else {
@@ -42,16 +42,29 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files() {
-  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAfRJzDL8KCpLPCCfPSzdfrqaKBcBXm6eA', NULL, '1.0', true);
+
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=yourKeyHere', NULL, '1.0', true);
+
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+
   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 
+
+
   wp_localize_script('main-university-js', 'universityData', array(
-    'root_url' => get_site_url()
+
+    'root_url' => get_site_url(),
+
+    'nonce' => wp_create_nonce('wp_rest')
+
   ));
+
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
