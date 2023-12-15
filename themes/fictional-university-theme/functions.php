e2d3c781a5334be5eee5eda0772062a1c2767c8f
Julian Chatterton <juleschatt@Julians-iMac.local>
2023-12-14 21:11:27 -0600

    troubleshooting4

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -112,6 +112,6 @@ function universityMapKey($api) {
   $api['key'] = 'AIzaSyAfRJzDL8KCpLPCCfPSzdfrqaKBcBXm6eA';
   return $api;
 }
-add_filter('acf/fields/google_map/api', 'universityMpKey');
+add_filter('acf/fields/google_map/api', 'universityMapKey');
 
 // test2345
